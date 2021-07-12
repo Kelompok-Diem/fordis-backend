d304ff875bb8f29910d273f45c5542323e2e7162
reyrizki <reynaldirizki97.rr@gmail.com>
2021-07-12 17:37:27 +0700

    chore(Images): Add image to server
    - Add API endpoint to get image
    - Ignore images file

--- api/controllers/imageController.js
+++ api/controllers/imageController.js
@@ -4,7 +4,7 @@ let path = require('path');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, `${__dirname}/../../../images/`);
+        cb(null, `${__dirname}/../../images/`);
     },
     filename: function (req, file, cb) {
         cb(null, uuidv4() + '-' + Date.now() + path.extname(file.originalname));
@@ -22,3 +22,7 @@ const fileFilter = (req, file, cb) => {
 }
 
 exports.upload = multer({ storage });
+
+exports.getImage = (req, res) => {
+    res.sendFile(path.join(__dirname, '../../images', req.params.filename));
+}
